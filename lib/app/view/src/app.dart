61c77269b8ba70f492db1fb83b9f24002df24e58
Ezra <ezracodes@gmail.com>
2024-05-12 19:19:48 +0800

    feat(app): add weather repo provider

--- lib/app/view/src/app.dart
+++ lib/app/view/src/app.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutther/app/app.dart';
 import 'package:flutther/home/home.dart';
 import 'package:flutther/l10n/l10n.dart';
+import 'package:weather_repository/weather_repository.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return RepositoryProvider(
+      create: (context) => WeatherRepository(),
+      child: const AppView(),
+    );
+  }
+}
+
+class AppView extends StatelessWidget {
+  const AppView({super.key});
 
   @override
   Widget build(BuildContext context) {
